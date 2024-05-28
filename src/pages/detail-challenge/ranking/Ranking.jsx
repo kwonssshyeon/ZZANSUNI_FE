8136415efb7e22dae89939db6187952b809b4bc1
kwonssshyeon <kwonsh4396@gmail.com>
2024-05-28 23:16:06 +0900

    Feat: 챌린지 랭킹 페이지 구현

--- src/pages/detail-challenge/ranking/Ranking.jsx
+++ src/pages/detail-challenge/ranking/Ranking.jsx
@@ -1,12 +1,25 @@
+import * as Styles from './styles';
+import UserItem from '../components/UserItem';
+
 const Ranking = () => {
 
 
     return (
-      <div>
-        나는 2 !
-      </div>
+      <>
+      <Styles.Wrapper>
+        <Styles.Category>에코</Styles.Category>
+        <Styles.Title>길에 떨어진 쓰레기 줍기 챌린지</Styles.Title>
+      </Styles.Wrapper>
+      <Styles.RankingWrapper>
+        <UserItem></UserItem>
+        <UserItem></UserItem>
+        <UserItem></UserItem>
+        <UserItem></UserItem>
+        <UserItem></UserItem>
+      </Styles.RankingWrapper>
+      </>
     );
   }
   
   
-  export default Ranking;
+export default Ranking;
--- src/pages/detail-challenge/ranking/styles.js
+++ src/pages/detail-challenge/ranking/styles.js
@@ -1,1 +1,31 @@
 import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+    margin: 30px;
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+`;
+
+export const Category = styled.div`
+    font-size: var(--font-size-xs);
+    color: var(--color-green-01);
+`;
+
+export const Title = styled.div`
+    font-size: var(--font-size-xl);
+    font-weight: bold;
+    margin-bottom: 15px;
+`;
+
+export const Text = styled.div`
+    font-size: var(--font-size-sm);
+`
+
+export const RankingWrapper = styled.div`
+    margin: 30px;
+    margin-top: 0px;
+    margin-bottom: 80px;
+    display: flex;
+    flex-direction: column;
+`;
