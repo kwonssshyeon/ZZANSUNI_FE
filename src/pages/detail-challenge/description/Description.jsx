b492b56dd9512a2b55fcfbb9e3fb2a789bc908f5
kwonssshyeon <kwonsh4396@gmail.com>
2024-05-28 22:21:15 +0900

    Feat: 챌린지 상세 설명 페이지(난이도 제외) 구현

--- src/pages/detail-challenge/description/Description.jsx
+++ src/pages/detail-challenge/description/Description.jsx
@@ -1,12 +1,28 @@
 import * as Styles from './styles';
 
-const Description = () => {
+const Description = ({data}) => {
 
 
   return (
-    <div>
-      나는 1 !
-    </div>
+    <Styles.Wrapper>
+      <Styles.Category>에코</Styles.Category>
+      <Styles.Title>길에 떨어진 쓰레기 줍기 챌린지</Styles.Title>
+      <Styles.Text>길에 떨어진 쓰레기를 주워 우리동네를 깨끗하게 만드는 챌린지입니다.</Styles.Text>
+      <Styles.Line />
+      <Styles.ContentWrapper>
+        <Styles.BoldText>마감일</Styles.BoldText>
+        <Styles.Text>2024.5.25 ~ 2024.5.27</Styles.Text>
+      </Styles.ContentWrapper>
+      ???
+      <Styles.Line />
+      <Styles.SubText>사회적 책임감을 높이고 환경 보호 의식을 증진하기 위한 운동 중 하나입니다. 이 챌린지는 일상에서 마주하는 길거리나 공원 등 공공장소에 흩뿌려진 쓰레기를 주변을 깨끗하게 만들기 위해 자발적으로 줍는 것을 촉구합니다.</Styles.SubText>
+      <Styles.Line />
+      <Styles.BoldText>참여방법</Styles.BoldText>
+      <Styles.Text>1. 쓰레기 줍기: 선택한 장소에서 쓰레기를 주웁니다. <br/>
+          2. 사진 찍기 및 공유: 쓰레기를 줍는 과정이나 결과물을 사진으로 찍어 인증하기 페이지에 업로드 합니다. <br/>
+          3. 다른 사람에게 공유하기: 쓰레기 줍기 챌린지에 참여한 후에는 친구, 가족 또는 동료에게 도전을 넘겨 다음 참여자를 유도합니다. <br/></Styles.Text>
+      <Styles.Btn>참여하기</Styles.Btn>
+    </Styles.Wrapper>
   );
 }
 
--- src/pages/detail-challenge/description/styles.js
+++ src/pages/detail-challenge/description/styles.js
@@ -1,1 +1,56 @@
 import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+    margin: 30px;
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+`;
+
+export const Category = styled.div`
+    font-size: var(--font-size-xs);
+    color: var(--color-green-01);
+`;
+
+export const Title = styled.div`
+    font-size: var(--font-size-xl);
+    font-weight: bold;
+    margin-bottom: 15px;
+`;
+
+export const Text = styled.div`
+    font-size: var(--font-size-sm);
+    margin: 5px 0;
+`
+
+export const BoldText = styled.div`
+    font-size: var(--font-size-sm);
+    font-weight: bold;
+    margin: 5px 0;
+`
+
+export const SubText = styled.div`
+    font-size: var(--font-size-sm);
+    color: var(--color-grey-01);
+`
+
+export const Line = styled.div`
+    border-top: 1px solid var(--color-green-06);
+    margin: 30px 0px;
+`
+
+export const ContentWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+`
+
+export const Btn = styled.button`
+    border-radius: 20px;
+    background-color: var(--color-green-01);
+    color: var(--color-white);
+    font-weight: bold;
+    width: 100%;
+    height: 50px;
+    border: none;
+    margin: 30px 0px;
+`
