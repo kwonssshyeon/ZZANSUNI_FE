0babfca8dbf28e58e46e9381baf5eead1a29beff
Dobbymin <gmin0701@knu.ac.kr>
2024-05-28 20:51:12 +0900

    feat: navigate Bar 하단 고정 스타일 적용 (수정중)

--- src/components/nav-bar/styles.js
+++ src/components/nav-bar/styles.js
@@ -1,18 +1,29 @@
 import styled from '@emotion/styled';
 
+export const MainContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+
+export const ContentWrapper = styled.div`
+  flex: 1;
+`;
+
 export const NavigateBarLayout = styled.div`
   display: flex;
+  padding: 0.8rem 0;
   justify-content: space-between;
   background-color: #f1f1f1;
 
   /** 하단 고정 */
-  position: fixed;
-  width: 100%;
-  bottom: 0;
-  left: 0;
+  /* position: fixed; */
+  /* transform: translateY(-100%); */
+  /* width: 100%; */
+  /* bottom: 0; */
+  /* left: 0; */
+  /* height: 3.4375rem; */
 
-  /* width: 24.375rem; */
-  height: 3.4375rem;
   justify-content: center;
   align-items: center;
   gap: 2.5rem;
