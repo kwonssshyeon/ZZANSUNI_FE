43e5f31a6b45b75d0cbae26dfdab14819a85b61f
Dobbymin <gmin0701@knu.ac.kr>
2024-05-30 03:33:26 +0900

    Feat: Dashboard 구현

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import Strick from '../main/components/Strick';
+import { TeerCurrent, TeerTotal } from '../main/components/styles';
+import Record from './components/Record';
+import UserInfo from './components/UserInfo';
+import * as Styles from './styles';
+import Header from '@/components/header/Header';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <>
+      <Header title='대시보드' />
+      <Styles.DashBoardLayout>
+        <UserInfo />
+        <Record />
+        <Strick />
+      </Styles.DashBoardLayout>
+    </>
+  );
 };
 
 export default Dashboard;
