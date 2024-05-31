53bc859fe29e8cd9ad30c4126b7732fb825546a0
kwonssshyeon <kwonsh4396@gmail.com>
2024-06-01 01:44:32 +0900

    Feat: 챌린지 리뷰 조회 라우팅 경로 설정

--- src/Router.jsx
+++ src/Router.jsx
@@ -10,7 +10,8 @@ import ChallengeDetailPage from '@/pages/detail-challenge/ChallengeDetailPage';
 import LoginPage from '@/pages/login/LoginPage';
 import MainPage from '@/pages/main/MainPage';
 import ChallengeRecordPage from '@/pages/record-challenge/ChallengeRecordPage';
-import WritingReview from './pages/review/writingReview/WritingReview';
+import WritingReview from '@/pages/review/writingReview/WritingReview';
+import ReviewList from '@/pages/review/review-list/ReviewList';
 
 export const Router = createBrowserRouter([
   {
@@ -64,6 +65,10 @@ export const Router = createBrowserRouter([
       {
         path: 'writing-reivew',
         element: <WritingReview />,
+      },
+      {
+        path: 'reivew-list',
+        element: <ReviewList />,
       },
     ],
   },
