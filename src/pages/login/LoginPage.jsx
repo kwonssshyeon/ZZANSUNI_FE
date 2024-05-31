5b0b946cdb04671211f314ad6df1831f2dc871f2
Dobbymin <gmin0701@knu.ac.kr>
2024-06-01 04:10:40 +0900

    Feat: 로그인 페이지 구현

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,5 +1,22 @@
+import { KAKAO_AUTH_URL } from '../../constants/URI';
+import * as Styles from './style';
+import Logo from '@/assets/login/kakao.svg';
+
 const LoginPage = () => {
-  return <div>안녕하세요</div>;
+  const HandlerKakaoLogin = () => {
+    window.location.href = KAKAO_AUTH_URL;
+  };
+
+  return (
+    <>
+      <Styles.ButtonSection>
+        <Styles.KaKaoLoginButton onClick={HandlerKakaoLogin}>
+          <Styles.KaKaoLoginLogo src={Logo} alt='kakao-symbol' />
+          <Styles.LoginText>카카오 로그인</Styles.LoginText>
+        </Styles.KaKaoLoginButton>
+      </Styles.ButtonSection>
+    </>
+  );
 };
 
 export default LoginPage;
