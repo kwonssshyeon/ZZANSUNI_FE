91bd3924b08afeb5c42e11398524aabb76b142b2
Dobbymin <gmin0701@knu.ac.kr>
2024-06-01 19:05:50 +0900

    Fix: 더미데이터 제거

--- src/pages/rank/components/UserRank.jsx
+++ src/pages/rank/components/UserRank.jsx
@@ -1,28 +1,42 @@
 import { Text } from '../styles';
 import * as Styles from './styles';
 import Profile from '@/assets/main/ZZAN-Profile.png';
+import { getTierDetails } from '@/constants/TierSchema';
 
-const UserRank = () => {
+const UserRank = ({ user, index }) => {
+  const { nickname, profileImageUrl, tierInfo, currentExp } = user;
+
+  const tierDetails = tierInfo
+    ? getTierDetails(tierInfo.tier)
+    : { color: 'var(--color-class-02)' };
+
+  console.log(tierDetails.color);
+
   return (
     <>
       <Styles.UserRankContainer>
         <Styles.Container>
-          <Text fw='700'>4위</Text>
+          <Styles.Text fw='700'>{index + 4}위</Styles.Text>
           <Styles.RankProfileImg src={Profile} width='1.5rem' height='1.5rem' />
-          <Text fw='700'>짠돌이</Text>
+          <Text fw='700'>{nickname}</Text>
         </Styles.Container>
         <Styles.Container>
           <Styles.Container>
-            <Text fw='700' fs='0.8rem' color='#C0C0C0'>
-              노비 II
-            </Text>
-            <Text fs='0.8rem' color='#C0C0C0'>
-              29
+            <Styles.Text
+              max='150px'
+              fw='700'
+              fs='0.8rem'
+              color={tierDetails.color}
+            >
+              {tierInfo.tier}
+            </Styles.Text>
+            <Text max='150px' fs='0.8rem' color={tierDetails.color}>
+              {tierInfo.currentExp}
             </Text>
           </Styles.Container>
           <Styles.Container>
             <Styles.TeerGraph>
-              <Styles.CurrentTeerGrpah />
+              <Styles.CurrentTeerGrpah background={tierDetails.color} />
             </Styles.TeerGraph>
           </Styles.Container>
         </Styles.Container>
