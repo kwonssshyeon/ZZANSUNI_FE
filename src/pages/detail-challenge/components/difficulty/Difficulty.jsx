e76a5b027b2e84648ecf9f54befff46ed5253998
kwonssshyeon <kwonsh4396@gmail.com>
2024-06-01 19:23:10 +0900

    Feat: 챌린지 난이도 리스트 api 연동

--- src/pages/detail-challenge/components/difficulty/Difficulty.jsx
+++ src/pages/detail-challenge/components/difficulty/Difficulty.jsx
@@ -1,6 +1,6 @@
 import * as Styles from './styles';
 
-const Difficulty = ({data}) => {
+const Difficulty = ({props, max}) => {
     return (
     <Styles.Outer>
         <Styles.RowWrapper>
@@ -11,26 +11,26 @@ const Difficulty = ({data}) => {
             <Styles.Wrapper>
                 <Styles.RowWrapper>
                     <Styles.Bar></Styles.Bar>
-                    <Styles.Text>8</Styles.Text>
+                    <Styles.Text>{props.difficulty}</Styles.Text>
                 </Styles.RowWrapper>
                 <Styles.RowWrapper>
                     <Styles.MaxBar></Styles.MaxBar>
-                    <Styles.SubText>10</Styles.SubText>
+                    <Styles.SubText>{max}</Styles.SubText>
                 </Styles.RowWrapper>
             </Styles.Wrapper>
         </Styles.RowWrapper>
         <Styles.BigMargin />
-        <Styles.Text>1주 / 3번 참여하기</Styles.Text>
+        <Styles.Text>1{props.dayType} / {props.participantCount}번 참여하기</Styles.Text>
         <Styles.BigMargin />
         <Styles.Wrapper>
             <Styles.RowWrapper>
                 <Styles.BoldText>참여 경험치</Styles.BoldText>
-                <Styles.ExpContent>20 포인트</Styles.ExpContent>
+                <Styles.ExpContent>{props.onceExp} 포인트</Styles.ExpContent>
             </Styles.RowWrapper>
             <Styles.Margin />
             <Styles.RowWrapper>
                 <Styles.BoldText>완료 경험치</Styles.BoldText>
-                <Styles.ExpContent>100 포인트</Styles.ExpContent>
+                <Styles.ExpContent>{props.successExp} 포인트</Styles.ExpContent>
             </Styles.RowWrapper>
         </Styles.Wrapper>
         <Styles.BigMargin />
