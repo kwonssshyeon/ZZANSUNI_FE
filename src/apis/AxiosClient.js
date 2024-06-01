08ce09b3bbc7ce4a70670607e02d5e507a37de04
kwonssshyeon <kwonsh4396@gmail.com>
2024-06-01 19:57:27 +0900

    !HOTFIX: authorization Bearer 키워드 추가

--- src/apis/AxiosClient.js
+++ src/apis/AxiosClient.js
@@ -14,7 +14,7 @@ axiosClient.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('accessToken');
     if (token) {
-      config.headers['Authorization'] = token;
+      config.headers['Authorization'] = "Bearer "+token;
     }
     return config;
   },
