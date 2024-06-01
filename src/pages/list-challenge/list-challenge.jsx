c8e43025a56dbe11eac4b190924b1873553826a8
Dobbymin <gmin0701@knu.ac.kr>
2024-06-02 02:38:00 +0900

    Feat: 챌린지 리스트 불러오기 구현

--- src/pages/list-challenge/list-challenge.jsx
+++ src/pages/list-challenge/list-challenge.jsx
@@ -1,4 +1,6 @@
-import { Container } from '../rank/components/styles';
+import React, { useEffect, useState } from 'react';
+
+import { getChallengeList } from '../../apis/list-challenge/list.challenge.api';
 import { Text } from '../rank/styles';
 import CategoryButton from './components/CategoryButton';
 import Contents from './components/Contents';
@@ -6,14 +8,34 @@ import * as Styles from './styles';
 import Header from '@/components/header/Header';
 
 const ChallengeList = () => {
+  const [listChallenges, setListChallenges] = useState([]);
+
+  useEffect(() => {
+    const fetchChallenges = async () => {
+      try {
+        const challenges = await getChallengeList(0, 10); // Example: fetch the first page with 10 items
+        setListChallenges(challenges.data.data);
+        console.log(listChallenges);
+      } catch (error) {
+        console.error('Error fetching challenges:', error);
+      }
+    };
+
+    fetchChallenges();
+  }, []);
+
   return (
     <>
       <Header title='챌린지 목록' BackgroundColor='#fff' />
       <Styles.ChallengeListLayout>
         <CategoryButton />
-        <Contents />
-        <Contents />
-        <Contents />
+        {listChallenges.length > 0 ? (
+          listChallenges.map((challenge) => (
+            <Contents key={challenge.id} challenge={challenge} />
+          ))
+        ) : (
+          <Text>챌린지를 불러올 수 없습니다.</Text> // Display a message if no listChallenges are available
+        )}
       </Styles.ChallengeListLayout>
     </>
   );
