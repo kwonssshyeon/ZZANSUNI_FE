16fd1b092e066153a149c367a30fe3680f659af0
Dobbymin <gmin0701@knu.ac.kr>
2024-06-01 14:11:57 +0900

    Feat: 임시 더미데이터 추가

---
+++ src/constants/Dummy.js
@@ -0,0 +1,16 @@
+const UserDummy = {
+  result: 'SUCCESS',
+  data: {
+    id: 1,
+    nickname: 'john_doe',
+    profileImageUrl: 'https://example.com/profile.jpg',
+    email: 'john.doe@example.com',
+    tierInfo: {
+      tier: '성골 I',
+      totalExp: 1200,
+      currentExp: 200,
+    },
+  },
+  message: 'Operation completed successfully',
+  errorCode: '',
+};
