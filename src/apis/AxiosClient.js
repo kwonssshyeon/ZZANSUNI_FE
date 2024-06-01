ce73d107e062117061d7259ecaa0943ed5739165
Dobbymin <gmin0701@knu.ac.kr>
2024-06-01 20:13:31 +0900

    Fix: 로그인 기능 구현

--- src/apis/AxiosClient.js
+++ src/apis/AxiosClient.js
@@ -14,12 +14,12 @@ axiosClient.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('accessToken');
     if (token) {
-      config.headers['Authorization'] = "Bearer "+token;
+      config.headers['Authorization'] = 'Bearer ' + token;
     }
     return config;
   },
   (error) => {
-    return Promise.reject(error);
+    return reject(error);
   }
 );
 
