29a64c1006b617cf318a7abc740e040f37a85240
김강민 <79429482+Dobbymin@users.noreply.github.com>
2024-06-08 00:27:37 +0900

    JS 프로젝트 삭제 (#53)
    * Hotfix: navigate bar 이동 경로 수정
    
    * Remove: JS 프로젝트 삭제

--- src/constants/URI.js
+++
@@ -1,6 +0,0 @@
-export const BASE_URI = import.meta.env.VITE_APP_BASE_URL;
-
-export const REST_API_KEY = import.meta.env.VITE_APP_KAKAO_API_KEY;
-export const REDIRECT_URI = import.meta.env.VITE_APP_REDIRECT_URI;
-
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`;
--- src/pages/main/style.js
+++
@@ -1,7 +0,0 @@
-import styled from '@emotion/styled';
-
-export const MainPageLayout = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  flex-direction: column;
-`;
